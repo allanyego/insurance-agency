d422c3b99b0fc350a8a68c65f2f7a6aca944b8e2
yegow <allanyego05@gmail.com>
2020-05-19 14:40:05 +0300

    Updates

---
+++ precache-manifest.7377a5f935edbefc5cac30e9a7ad60c4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141b8827ee344668efe57d68bb106734",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34ae83db22bcf6311ba7",
+    "url": "./static/css/0.32fffb6f.chunk.css"
+  },
+  {
+    "revision": "234a10d2fd565a804565",
+    "url": "./static/css/1.13af234c.chunk.css"
+  },
+  {
+    "revision": "ce0cf44694aa0780baf6",
+    "url": "./static/css/4.0670b5fa.chunk.css"
+  },
+  {
+    "revision": "cd2900cdaca888719b25",
+    "url": "./static/css/5.09a89592.chunk.css"
+  },
+  {
+    "revision": "cc7de3868ece333e5191",
+    "url": "./static/css/6.fcd85223.chunk.css"
+  },
+  {
+    "revision": "b57cb7522863b56f6701",
+    "url": "./static/css/7.eb32eee5.chunk.css"
+  },
+  {
+    "revision": "4ed5d6bd9f264afd059c",
+    "url": "./static/css/8.eb32eee5.chunk.css"
+  },
+  {
+    "revision": "8ffd2fcc03e0cc6ce846",
+    "url": "./static/css/main.133407ca.chunk.css"
+  },
+  {
+    "revision": "34ae83db22bcf6311ba7",
+    "url": "./static/js/0.d7cec442.chunk.js"
+  },
+  {
+    "revision": "234a10d2fd565a804565",
+    "url": "./static/js/1.bcb8944b.chunk.js"
+  },
+  {
+    "revision": "ce0cf44694aa0780baf6",
+    "url": "./static/js/4.a6c5fe32.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/4.a6c5fe32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2900cdaca888719b25",
+    "url": "./static/js/5.649befe3.chunk.js"
+  },
+  {
+    "revision": "cc7de3868ece333e5191",
+    "url": "./static/js/6.df611347.chunk.js"
+  },
+  {
+    "revision": "b57cb7522863b56f6701",
+    "url": "./static/js/7.39caa92a.chunk.js"
+  },
+  {
+    "revision": "4ed5d6bd9f264afd059c",
+    "url": "./static/js/8.70bac0f6.chunk.js"
+  },
+  {
+    "revision": "8ffd2fcc03e0cc6ce846",
+    "url": "./static/js/main.f472b71a.chunk.js"
+  },
+  {
+    "revision": "57ee93c043740a8c95dc",
+    "url": "./static/js/runtime-main.690e3a80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orient-insurance/precache-manifest.b5a8de959030a2fc16046e89b6c51d4a.js"
+  "./precache-manifest.7377a5f935edbefc5cac30e9a7ad60c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/orient-insurance/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
